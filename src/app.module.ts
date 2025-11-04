3511eea6f4fba0cb0ada28e7275b9887307c3bc1
hay-dev2024 <hay.dev2024@gmail.com>
2025-11-04 11:35:08 +0900

    feat: create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { NavigationModule } from './navigation/navigation.module';
+import { HistoryModule } from './history/history.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule, AuthModule, NavigationModule, HistoryModule, CommonModule
+  ],
   controllers: [],
   providers: [],
 })
