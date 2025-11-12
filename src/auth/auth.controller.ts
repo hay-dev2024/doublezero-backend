ace05d51c73b2409089e5567e1121b368d3d3ec4
hay-dev2024 <hay.dev2024@gmail.com>
2025-11-12 17:29:06 +0900

    feat: add Post method in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { GoogleLoginDto } from './dto/google-login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    // POST /auth/google - Google Login
+    @Post('goolge')
+    async googleLogin(@Body() googleLoginDto: GoogleLoginDto) {
+        return this.authService.validateGoogleToken(googleLoginDto.idToken);
+    }
+}
