039cd83d863a53b20c20b0605ffe799b236f47f6
hay-dev2024 <hay.dev2024@gmail.com>
2025-11-13 14:41:02 +0900

    feat: add test-token endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GoogleLoginDto } from './dto/google-login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly authService: AuthService,
+        private readonly jwtService: JwtService,
+    ) {}
 
     // POST /auth/google - Google Login
-    @Post('goolge')
+    @Post('google')
     async googleLogin(@Body() googleLoginDto: GoogleLoginDto) {
         return this.authService.validateGoogleToken(googleLoginDto.idToken);
     }
+
+    /** 
+     * JWT Test 
+     * GET /auth/test-token?userId={userId}&email={email}
+    */
+   @Get('test-token')
+   async getTestToken(
+    @Query('userId') userId: string,
+    @Query('email') email: string,
+   ) {
+    // Temp JWT
+    const accessToken = this.jwtService.sign({
+        sub: userId || '507f1f77bcf86cd799439011',
+        email: email || 'test@mail.com',
+    });
+
+    return {
+        accessToken,
+        message: 'Test Token. Remove this endpoint before diployment.'
+    };
+   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '7d',
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
         },
       }),
       inject: [ConfigService],
